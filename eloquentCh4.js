ad0ae24da42c4ec1d13ae7da7749d76f9621d5c9
John McKirdy <57973241+jmckirdy@users.noreply.github.com>
2020-10-27 20:12:08 -0700

    added tableFor loop, added some params into the journal, getting error that length is undefined

--- eloquentCh4.js
+++ eloquentCh4.js
@@ -1,4 +1,4 @@
-let journal = []
+let journal = [76, 9, 4, 1, false]
 
 function addEntry(events, squirrel) {
     journal.push({events, squirrel});
@@ -13,5 +13,18 @@ function phi(table) {
         (table[0] + table[2]))
 }
 
-console.log(phi([76, 9, 4, 1]));
+function tableFor(event, journal) {
+    let table = [0, 0, 0, 0];
+    for (let i = 0; i < journal.length; i++) {
+        let entry = journal[i], index = 0;
+        if (entry.events.includes(event)) index += 1;
+        if (entry.squirrel) index += 2;
+        table[index] +=1;
+    }
+    return table;
+}
+
+console.log(tableFor(['pizza', journal]));
+
+
 
