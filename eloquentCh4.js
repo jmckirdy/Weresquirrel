6cd36656beb9ff64ada5b23e4b28a5642407559a
John McKirdy <57973241+jmckirdy@users.noreply.github.com>
2020-10-27 19:56:59 -0700

    added jounal array, and addEvents function

--- eloquentCh4.js
+++ eloquentCh4.js
@@ -1,3 +1,9 @@
+let journal = []
+
+function addEntry(events, squirrel) {
+    journal.push({events, squirrel});
+}
+
 function phi(table) {
     return( (table[3] * table[0]) - (table[2] * table[1])) /
     Math.sqrt( 
